777295fbfc70de29fdcd3d073667823068bac2f6
cdjohnson97 <cdmilandou@gmail.com>
2020-12-09 13:24:59 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Johnson"/>
-  </div>
+	<div id="app">
+		<HelloWorld msg="Johnson" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
@@ -16,12 +16,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
